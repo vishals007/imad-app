92461f0e26a1d4a69b1d18538c90f2b5be11d16b
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-07 08:55:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.get('/article-one', function(req,res){
 });
 
 app.get('/article-two', function(req,res){
-    res.send('Article two requested and will be serverd here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
     
 });
 
 app.get('/article-three', function(req,res){
-    res.send('Article three requested and will be serverd here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
     
 });
 

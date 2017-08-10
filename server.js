e205e726ac541b5102f67b49b4f90b22401e0ffc
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-10 11:54:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.send(createTemplate(articelone));
+    res.send(createTemplate(articleone));
     
 });
 

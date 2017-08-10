22039e749cdffccbd400a3088c81aa783acd15f2
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-10 16:30:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,10 +101,13 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80

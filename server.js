a880b287bfb82bc3319885677e6810e6dba1a76a
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-10 16:28:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,8 +97,8 @@ app.get('/:articleName', function(req,res){
                                                     
                                                 });*/
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {

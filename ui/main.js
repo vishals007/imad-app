f22fad0b1382bbbe1fc67f442194ed91c7028e50
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-10 17:54:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ element.innerHTML = 'New Value';
 
 //Move the image
 var img = document.getElementById('madi');
-img.oneclick = function() {
-    img.style.marginleft = '100px';
+img.oneclick = function () {
+    img.style.marginLeft = '100px';
 };

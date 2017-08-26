6c3ed9d6a49f27db24018e90c9f20de4455757c7
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-26 17:07:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool= require('pg').Pool;
+
+var config = {
+   user: 'vishal14shetty',
+   database:'vishal14shetty',
+   host: 'db.imad.hasura-app.io',
+   port: '5432',
+password: process.env.DB_PASSWORD
+
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -75,27 +85,88 @@ function createTemplate(data) {
    `;
    return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName', function(req,res){
+var pool= new Pool(config);
+app.get('/test-db', function (req, res)
+{
+// make a select request
+// return a response with the results
+pool.query("SELECT * FROM test", function(err, result)
+{
+if (err)
+{
+res.status(500).send (err.toString());
+}
+else
+{ 
+res.send(JSON.stringify(result.rows));
+}
+});
+});
+
+var counter=0;
+app.get('/counter', function (req, res)
+{
+counter=counter+1;
+res.send(counter.toString());
+});
+var names=[];
+app.get('/submit-name', function (req, res)
+{
+// URL:/submit-name?name=xxxxx
+// Get the name from the request
+var name= req.query.name; //ToDO
+names.push(name);
+//JSON:JavaScript Object Notation
+
+res.send(JSON.stringify(names)); //ToDo
+
+});
+
+var articles=[];
+app.get('/articles/:articleName', function (req, res)
+{
+// articleName==article-one
+// articles[articleName]= { }content object of article-one
+
+//'SELECT * FROM article WHERE title='article-one' 
+pool.query("SELECT * FROM article WHERE title=$1", [req.params.articleName], function (err,result)
+{
+if(err)
+{
+res.status(500).send (err.toString());
+}
+else
+{
+if (result.rows.length===0)
+{
+res.status(404).send('Article Not Found'); 
+}
+else
+{
+var articleData=result.rows[0];
+res.send(createTemplate(articleData));
+}
+}
+{
+
+  }
+});
+
+});
+
+/*app.get('/:articleName', function(req,res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
     
-});
+});*/
 
-                                                /*app.get('/article-two', function(req,res){
-                                                    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-                                                    
-                                                });
-                                                
-                                                app.get('/article-three', function(req,res){
-                                                    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-                                                    
-                                                });*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
@@ -105,6 +176,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res)
+{
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80

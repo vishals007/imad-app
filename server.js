a3c999dfe3eba23095bf8a9f28b73b0ffd19eae9
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-29 14:05:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ function hash (input,salt){
 }
 
 app.get('/hash/:input',function(req,res) {
-   var hashedString = hash(req.params.input,'this-is');
+   var hashedString = hash(req.params.input,'this-is-some-random-string');
    res.send(hashedString);
 });
 

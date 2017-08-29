d247381890589bfada5349eb726efca1f3aef6df
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-29 13:32:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 function hash (input,salt){
     //how do we create a hash
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex')
+    return hashed.toString('hex');
 }
 
 app.get('/hash:input',function(req,res) {

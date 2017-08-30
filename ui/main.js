ae9896210e449a145d934b3d36872955a4ed81bf
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-30 16:11:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,6 +35,8 @@ submit.onclick = function () {
   var names = ['name1','name2','name3','name4'];
   var list = '';
   for (var i=0; i<names.length; i++) {
-      list += '<li>' + names[1] + '</li>';
+      list += '<li>' + names[i] + '</li>';
   }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
 };

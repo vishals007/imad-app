025ba0932858299f5c13b0db66be7359d2c0a36c
vishals007 <30680351+vishals007@users.noreply.github.com>
2017-08-30 17:12:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 //counter code
 var button = document.getElementById('counter');
 
-
 button.onclick = function () {
   
   //create a request object
@@ -19,10 +18,10 @@ button.onclick = function () {
       }
       //not done yet
   };
+  
   //Make the request
   request.open('GET','http://vishal14shetty.imad.hasura-app.io/counter',true);
   request.send(null);
-  
 };
 
 //submit name
@@ -30,14 +29,31 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-  //make a request to the server and send the name
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variablle
+    request.onreadystatechange = function() {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          //take some action
+          if (request.status === 200) {
+         //capture a list of names and render it as a list
+       var names = request.responseText;
+       names = JSON.parse(names);
+       var list = '';
+       for (var i=0; i<names.length; i++) {
+         list += '<li>' + names[i] + '</li>';
+        } 
+        var ul = document.getElementById('namelist');
+        ul.innerHTML = list; 
+      }
+    }
+      //not done yet
+  };
   
-  //capture a list of names and render it as a list
-  var names = ['name1', 'name2', 'name3','name4'];
-  var list = '';
-  for (var i=0; i<names.length; i++) {
-      list += '<li>' + names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+  //Make the request
+  request.open('GET','http://vishal14shetty.imad.hasura-app.io/submit-name?name' + name, true);
+  request.send(null);
+  
 };
